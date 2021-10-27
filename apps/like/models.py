1a5ce6663c514268569b26e6fcf9900357a432cc
claudio.siervi <claudio.siervi@gmail.com>
2021-10-27 02:28:23 -0300

    add model like

--- apps/like/models.py
+++ apps/like/models.py
@@ -1,3 +1,21 @@
 from django.db import models
+from django.core.validators import MaxValueValidator, MinValueValidator
 
-# Create your models here.
+from core.mixin.model import AbstractModelMixin
+from apps.user.models import User
+from apps.post.models import Post
+
+
+class Like(AbstractModelMixin, models.Model):
+    rating = models.PositiveSmallIntegerField(
+        verbose_name="Rating post",
+        validators=[MinValueValidator(1), MaxValueValidator(5)],
+        editable=False,
+    )
+    post = models.ForeignKey(Post, related_name="likes", on_delete=models.CASCADE)
+    owner = models.ForeignKey(User, related_name="likes", on_delete=models.CASCADE)
+
+    class Meta:
+        db_table = "like"
+        ordering = ["created_at"]
+
